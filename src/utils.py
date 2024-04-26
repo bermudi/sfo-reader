9456203d97f3165fad4fa8073587db61ab00f821
bermudi <github.igizp@dabg.uk>
2024-04-26 17:09:52 -0600

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,7 @@ def cleanup_title (title):
     title = title.replace("\n", "")
     title = title.replace("®", "")
     title = title.replace("™", "")
-    title = title.replace(":", " -")
+    title = title.replace(":", " - ")
     while "  " in title:
         title = title.replace("  ", " ")
     return title
